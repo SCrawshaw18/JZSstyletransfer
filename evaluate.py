f7961edb4a81c7e23754dc4ffc550f4323e73490
wangjessicax <wang.jessicax@gmail.com>
2018-03-09 17:10:47 -0600

    hjk

--- evaluate.py
+++ evaluate.py
@@ -11,6 +11,8 @@ import time
 import json
 import subprocess
 import numpy
+import imageio
+imageio.plugins.ffmpeg.download()
 from moviepy.editor import *
 from moviepy.video.io.VideoFileClip import VideoFileClip
 import moviepy.video.io.ffmpeg_writer as ffmpeg_writer
@@ -72,6 +74,7 @@ def ffwd_video(path_in, path_out, checkpoint_dir, device_t='/gpu:0', batch_size=
 
 # get img_shape
 def ffwd(data_in, paths_out, checkpoint_dir, device_t='/gpu:0', batch_size=4):
+
     assert len(paths_out) > 0
     is_paths = type(data_in[0]) == str
     if is_paths:
@@ -105,7 +108,7 @@ def ffwd(data_in, paths_out, checkpoint_dir, device_t='/gpu:0', batch_size=4):
         else:
             saver.restore(sess, checkpoint_dir)
 
-        num_iters = int(len(paths_out)/batch_size)
+        num_iters = int(len(paths_out)/batch_size) #1
        
         for i in range(num_iters): #num_iters is 1
             pos = i * batch_size
@@ -128,7 +131,7 @@ def ffwd(data_in, paths_out, checkpoint_dir, device_t='/gpu:0', batch_size=4):
                 save_img(path_out, _preds[j])
         
               
-        remaining_in = data_in[num_iters*batch_size:]
+        remaining_in = data_in[num_iters*batch_size:] #matrix with length of 1
         remaining_out = paths_out[num_iters*batch_size:]
         print("this is remainings")
         print(len(remaining_in))
@@ -140,65 +143,7 @@ def ffwd(data_in, paths_out, checkpoint_dir, device_t='/gpu:0', batch_size=4):
         ffwd(remaining_in, remaining_out, checkpoint_dir, 
             device_t=device_t, batch_size=1)
 
-def ffwd_mod(data_in, paths_out, checkpoint_dir="scream.ckpt", device_t='/gpu:0', batch_size=4):
-    assert len(paths_out) > 0
-    is_paths = type(data_in[0]) == str
-    if is_paths:
-        assert len(data_in) == len(paths_out)
-        img_shape = get_img(data_in[0]).shape
-    else:
-        assert data_in.size[0] == len(paths_out)
-        img_shape = X[0].shape
 
-    g = tf.Graph()
-    batch_size = min(len(paths_out), batch_size)
-    curr_num = 0
-    soft_config = tf.ConfigProto(allow_soft_placement=True)
-    soft_config.gpu_options.allow_growth = True
-    with g.as_default(), g.device(device_t), \
-            tf.Session(config=soft_config) as sess:
-        batch_shape = (batch_size,) + img_shape
-        img_placeholder = tf.placeholder(tf.float32, shape=batch_shape,
-                                         name='img_placeholder')
-
-        preds = transform.net(img_placeholder)
-        saver = tf.train.Saver()
-        if os.path.isdir(checkpoint_dir):
-            ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
-            if ckpt and ckpt.model_checkpoint_path:
-                saver.restore(sess, ckpt.model_checkpoint_path)
-            else:
-                raise Exception("No checkpoint found...")
-        else:
-            saver.restore(sess, checkpoint_dir)
-
-        num_iters = int(len(paths_out)/batch_size)
-       
-        for i in range(num_iters): #num_iters is 1
-            pos = i * batch_size
-            curr_batch_out = paths_out[pos:pos+batch_size]
-            if is_paths:
-                curr_batch_in = data_in[pos:pos+batch_size]
-                X = np.zeros(batch_shape, dtype=np.float32)
-                for j, path_in in enumerate(curr_batch_in):
-                    img = get_img(path_in)
-                    assert img.shape == img_shape, \
-                        'Images have different dimensions. ' +  \
-                        'Resize images or use --allow-different-dimensions.'
-                    X[j] = img
-            else:
-                X = data_in[pos:pos+batch_size]
-
-            _preds = sess.run(preds, feed_dict={img_placeholder:X})
-            for j, path_out in enumerate(curr_batch_out):
-                save_img(path_out, _preds[j])
-        
-              
-        remaining_in = data_in[num_iters*batch_size:]
-        remaining_out = paths_out[num_iters*batch_size:]
-    if len(remaining_in) > 0:
-        ffwd(remaining_in, remaining_out, checkpoint_dir, 
-            device_t=device_t, batch_size=1)
 def ffwd_to_img(in_path, out_path, checkpoint_dir, device='/cpu:0'):
     paths_in, paths_out = [in_path], [out_path]
     ffwd(paths_in, paths_out, checkpoint_dir, batch_size=1, device_t=device)
@@ -276,7 +221,7 @@ def main():
         if opts.allow_different_dimensions:
             ffwd_different_dimensions(full_in, full_out, opts.checkpoint_dir, 
                     device_t=opts.device, batch_size=opts.batch_size)
-        else:
+        else :
             ffwd(full_in, full_out, opts.checkpoint_dir, device_t=opts.device,
                     batch_size=opts.batch_size)
 
